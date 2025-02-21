31314f403bbcec1c2e1499dbbb531e54c8c7960d
Muhammed Salih Gümüş <gumusmuhammedsalih@gmail.com>
2025-02-21 16:59:42 +0300

    Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import { HydrateClient } from "~/trpc/server";
+import { api, HydrateClient } from "~/trpc/server";
 import { AuthButtons } from "./_components/auth-buttons";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await api.posts.getAll();
+
   return (
     <HydrateClient>
       <main>
         <AuthButtons />
+        <div>
+          {posts.map((post) => (
+            <div key={post.id}>
+              <p>{post.content}</p>
+            </div>
+          ))}
+        </div>
       </main>
     </HydrateClient>
   );
